626217b98bcbc1510313b21337b5261e832905f0
Chirag <me@chi6rag.net>
2018-03-25 17:23:06 +0530

    Gossiping among multiple nodes

--- prattle_test.go
+++ prattle_test.go
@@ -1,8 +1,10 @@
 package prattle
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPrattleWithMoreThanOneNode(t *testing.T) {
@@ -65,16 +67,24 @@ func TestSetWhenKeyAlreadyExist(t *testing.T) {
 	defer prattle.Shutdown()
 }
 
+// TODO: Refactor test - remove sleep
 func TestWhenNewNodeJoinsPrattleCluster(t *testing.T) {
 	prattleOne, errOne := NewPrattle("", 9000)
 	assert.Nil(t, errOne)
 	prattleTwo, errTwo := NewPrattle("", 9001)
 	assert.Nil(t, errTwo)
-	assert.Equal(t, 1, prattleOne.members.NumMembers())
-	errThree := prattleTwo.JoinCluster("0.0.0.0:9000")
+	prattleThree, errThree := NewPrattle("", 9002)
 	assert.Nil(t, errThree)
-	assert.Equal(t, 2, prattleOne.members.NumMembers())
-	assert.Equal(t, 2, prattleTwo.members.NumMembers())
 	defer prattleOne.Shutdown()
 	defer prattleTwo.Shutdown()
+	defer prattleThree.Shutdown()
+	assert.Equal(t, 1, prattleThree.members.NumMembers())
+	errFour := prattleTwo.JoinCluster("0.0.0.0:9000")
+	assert.Nil(t, errFour)
+	errFive := prattleThree.JoinCluster("0.0.0.0:9000")
+	assert.Nil(t, errFive)
+	time.Sleep(200 * time.Millisecond)
+	assert.Equal(t, 3, prattleOne.members.NumMembers())
+	assert.Equal(t, 3, prattleTwo.members.NumMembers())
+	assert.Equal(t, 3, prattleThree.members.NumMembers())
 }
